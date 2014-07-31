945d41bd0cc66a3844b5ccedb66c83c0e7ebf68b
Peter Josling <peterjosling@gmail.com>
2014-07-31 18:31:16 +0100

    Implement basic functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function TraceurCompiler(config) {
+var traceur = require('traceur'),
+	path = require('path');
 
+function TraceurCompiler(config) {
+	this.config = config
 }
 
 TraceurCompiler.prototype.brunchPlugin = true;
@@ -7,7 +10,20 @@ TraceurCompiler.prototype.type = 'javascript';
 TraceurCompiler.prototype.extension = 'js';
 
 TraceurCompiler.prototype.compile = function(data, path, callback) {
+	var es5 = traceur.compile(data, {
+		sourceMaps: this.config.sourceMaps,
+		filename: path,
+		modules: false
+	});
 
+	callback(null, {
+		data: es5.js,
+		map: es5.generatedSourceMap
+	});
 };
+
+TraceurCompiler.prototype.include = [
+	path.join(__dirname, 'node_modules', 'traceur', 'bin', 'traceur-runtime.js')
+];
 
 module.exports = TraceurCompiler;
