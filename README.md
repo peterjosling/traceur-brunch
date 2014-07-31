f162eccdab69cc7a1c1bc7ca8faec74e62fc0579
Peter Josling <peterjosling@gmail.com>
2014-07-31 15:52:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# traceur-brunch
+
+Adds [Traceur](https://github.com/google/traceur-compiler) ES6 transpiler support to [brunch](http://brunch.io).
---
+++ index.js
@@ -0,0 +1,13 @@
+function TraceurCompiler(config) {
+
+}
+
+TraceurCompiler.prototype.brunchPlugin = true;
+TraceurCompiler.prototype.type = 'javascript';
+TraceurCompiler.prototype.extension = 'js';
+
+TraceurCompiler.prototype.compile = function(data, path, callback) {
+
+};
+
+module.exports = TraceurCompiler;
